3ec77e9ecf77ec19ac69046ac9d7b0f3e1747260
Arkan Aulia Farhan <32859227+arkanaulia@users.noreply.github.com>
2022-12-31 17:19:07 +0700

    error skeleton

--- components/Landing.jsx
+++ components/Landing.jsx
@@ -3,7 +3,7 @@ import { Suspense } from "react";
 
 export default function Landing() {
   return (
-    <Suspense fallback={<loading />}>
+    <Suspense fallback="loading...">
       <div className="w-full h-full absolute">
       <Spline scene="https://prod.spline.design/baG5ZVH4xAfQ9a0S/scene.splinecode" /></div>
     </Suspense>
